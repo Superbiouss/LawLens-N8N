1b8679f7e729d77acb3541d8b82cb861467ca254
Superbious <mr.superbious@gmail.com>
2026-04-02 00:35:01 +0530

    Initial Landing Page Connection with Dashboard.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     port: 5173,
     open: false,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        app: resolve(__dirname, 'app/index.html')
+      }
+    }
+  }
 });
